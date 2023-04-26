b10c397eb96105bc7cab5d945865183b9b148ee7
rbneiman <alecneiman@gmail.com>
2023-04-25 21:07:53 -0500

    cloud stuff

--- src/shaders.rs
+++ src/shaders.rs
@@ -295,10 +295,9 @@ const mat3 ROT_MAT = mat3(
     vec3(-0.28627997636795044, 0.7056889533996582, 0.6481102705001831),
     vec3(0.9133245348930359, -0.0034793566446751356, 0.4072175621986389)
 );
+const vec3 LIGHT_DIR = normalize(vec3(-1, 1, -1));
 
 
-//https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
-const float PHI = 1.61803398874989484820459;  // Φ = Golden Ratio
 float hash(vec2 p){
         p  = 50.0*fract( p*0.3183099  + vec2(0.71,0.113));
         return -1.0+2.0*fract( p.x*p.y*(p.x+p.y) );
@@ -320,56 +319,104 @@ float smoothmix(float a0, float a1, float w) {
         return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
 }
 
-const float PERIODS[] = float[](4.0,2.0,1.0,0.5);
-const float WEIGHTS[] = float[](1.0,0.5,0.25,0.135);
+const float PERIODS[] = float[](40.0,20.0,1.0,5.0);
+const float WEIGHTS[] = float[](1.0,0.5,0.25,0.0135);
 
 float perlin3D(vec3 pos){
 
-        float val = 0.0;
-        for(int i=0; i<1; ++i){
-            float weight = WEIGHTS[i];
-            float freq = 1.0/PERIODS[i];
+    float val = 0.0;
+    for(int i=0; i<1; ++i){
+        float weight = WEIGHTS[i];
+        float freq = 1.0/PERIODS[i];
 
-            vec3 noisePos = pos * freq;
-            vec3 floorPos = floor(noisePos);
-            vec3 fracPos = fract(noisePos);
+        vec3 noisePos = pos * freq;
+        vec3 floorPos = floor(noisePos);
+        vec3 fracPos = fract(noisePos);
 
-            vec2 sampleVec = vec2(0.0, 1.0);
+        vec2 sampleVec = vec2(0.0, 1.0);
 
-            float p1 = dot(unit_vec(floorPos), noisePos - floorPos);
-            float p2 = dot(unit_vec(floorPos + sampleVec.xxy), noisePos - sampleVec.xxy - floorPos);
-            float p3 = dot(unit_vec(floorPos + sampleVec.xyx), noisePos - sampleVec.xyx - floorPos);
-            float p4 = dot(unit_vec(floorPos + sampleVec.xyy), noisePos - sampleVec.xyy - floorPos);
+        float p1 = dot(unit_vec(floorPos), noisePos - floorPos);
+        float p2 = dot(unit_vec(floorPos + sampleVec.xxy), noisePos - sampleVec.xxy - floorPos);
+        float p3 = dot(unit_vec(floorPos + sampleVec.xyx), noisePos - sampleVec.xyx - floorPos);
+        float p4 = dot(unit_vec(floorPos + sampleVec.xyy), noisePos - sampleVec.xyy - floorPos);
 
-            float p5 = dot(unit_vec(floorPos + sampleVec.yxx), noisePos - sampleVec.yxx - floorPos);
-            float p6 = dot(unit_vec(floorPos + sampleVec.yxy), noisePos - sampleVec.yxy - floorPos);
-            float p7 = dot(unit_vec(floorPos + sampleVec.yyx), noisePos - sampleVec.yyx - floorPos);
-            float p8 = dot(unit_vec(floorPos + sampleVec.yyy), noisePos - sampleVec.yyy - floorPos);
+        float p5 = dot(unit_vec(floorPos + sampleVec.yxx), noisePos - sampleVec.yxx - floorPos);
+        float p6 = dot(unit_vec(floorPos + sampleVec.yxy), noisePos - sampleVec.yxy - floorPos);
+        float p7 = dot(unit_vec(floorPos + sampleVec.yyx), noisePos - sampleVec.yyx - floorPos);
+        float p8 = dot(unit_vec(floorPos + sampleVec.yyy), noisePos - sampleVec.yyy - floorPos);
 
-            float s1 = smoothmix(p1, p5, fracPos.x);
-            float s2 = smoothmix(p2, p6, fracPos.x);
-            float s3 = smoothmix(p3, p7, fracPos.x);
-            float s4 = smoothmix(p4, p8, fracPos.x);
+        float s1 = smoothmix(p1, p5, fracPos.x);
+        float s2 = smoothmix(p2, p6, fracPos.x);
+        float s3 = smoothmix(p3, p7, fracPos.x);
+        float s4 = smoothmix(p4, p8, fracPos.x);
 
-            float s5 = smoothmix(s1, s3, fracPos.y);
-            float s6 = smoothmix(s2, s4, fracPos.y);
+        float s5 = smoothmix(s1, s3, fracPos.y);
+        float s6 = smoothmix(s2, s4, fracPos.y);
 
 
-            //val += weight * p2;
-            val += weight * smoothmix(s5, s6, fracPos.z);
-        }
-        return val * 0.5 + 0.5;
+        //val += weight * p2;
+        val += weight * smoothmix(s5, s6, fracPos.z);
+    }
+    return (clamp(val, 0.0, 1.0) * 2.0) - 1.0;
+}
+
+
+const float THRESH = 0.01;
+float cloudMarch(vec3 rayPos, vec3 rayDir){
+
+
+    float t = 0.0;
+    float dist;
+    float th;
+    for(int i=0; i<150; ++i){
+        vec3 pos = rayPos + t * rayDir;
+        dist = perlin3D(pos);
+        th = t* THRESH;// * (hash(rayDir.xy)*0.05+0.95);
+        if(dist < th || dist > 500.0) break;
+        t += dist;
+    }
+
+
+    if(dist< th){
+        return t;
+    }else{
+        return -1.0;
+    }
+}
+
+vec3 cloudNormal(vec3 pos){
+    vec2 delta = vec2(0.0001, 0.0);
+    return normalize(vec3(
+        perlin3D(pos + delta.xyy) - perlin3D(pos - delta.xyy),
+        perlin3D(pos + delta.yxy) - perlin3D(pos - delta.yxy),
+        perlin3D(pos + delta.yyx) - perlin3D(pos - delta.yyx)
+        ));
 }
 
 void main () {
     vec3 rayDir = normalize(rayDirFrag);
     vec3 rayPos = rayPosFrag;
 
-    vec3 zeroed = vec3(rayPos.xy, 0.0);
-    float noise = perlin3D(ROT_MAT*rayPos + rayDir*100.0);
+//    vec3 zeroed = vec3(rayPos.xy, 0.0);
+    float dist = cloudMarch(rayPos, rayDir);
+    vec3 finalRayPos = rayPos + rayDir * dist;
+
+    if(dist < 0.0){
+        gl_FragDepth = 1.0;
+        fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+//            clamp(), 0.0, 1.0-BG), 1.0);
+    }else{
+        vec4 projCoords = viewProjMat * vec4(finalRayPos, 1.0);
+        gl_FragDepth = ((projCoords.z / projCoords.w) + 1.0) * 0.5;
+        vec3 normal = cloudNormal(finalRayPos);
+        fragColor = vec4(vec3(1.0, 1.0, 1.0)
+        * clamp(dot(normal, normalize(LIGHT_DIR)), 0.01, 1.0)
+        , 1.0);
+    }
+//    float noise = perlin3D(ROT_MAT*rayPos + rayDir*10.0);
     //perlin3D(zeroed + 100.0 * vec3(uv, 0.0));
 //    float noise3d = rand3D(rayPos.xyz + 100.0 * rayDir);
 //    vec3 vec = unit_vec(rayPos + 100.0 * rayDir);
-    fragColor = vec4(vec3(noise), 1.0);
+//    fragColor = vec4(vec3(cloud), 1.0);
 }
 "#;
